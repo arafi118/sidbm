4230331ba221ac19c265df831126e4b8def02eff
arafi118 <arp46301@gmail.com>
2023-08-07 09:35:56 +0700

    auth

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,7 +12,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        dd($request);
+        dd($request->expectsJson());
         return $request->expectsJson() ? null : route('/');
     }
 }
