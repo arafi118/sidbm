0cc55631ee4142b2e134d673e696fad81f20320a
arafi118 <arp46301@gmail.com>
2023-10-16 09:20:38 +0700

    Update Inventaris

--- public/migrasi.php
+++ public/migrasi.php
@@ -97,10 +97,10 @@ if (isset($_POST['copy'])) {
     mysqli_query($koneksi, $trigger_update);
     mysqli_query($koneksi, $trigger_delete);
 
-    mysqli_query($koneksi, "UPDATE inventaris_$lokasi SET kategori='1', jenis='1' WHERE kategori='1' AND jenis='3'");
-    mysqli_query($koneksi, "UPDATE inventaris_$lokasi SET kategori='1', jenis='3' WHERE kategori='5' AND jenis='5'");
-    mysqli_query($koneksi, "UPDATE inventaris_$lokasi SET kategori='2', jenis='3' WHERE kategori='6' AND jenis='5'");
-    mysqli_query($koneksi, "UPDATE inventaris_$lokasi SET kategori='3', jenis='3' WHERE kategori='7' AND jenis='5'");
+    mysqli_query($koneksi, "UPDATE dbm_laravel.inventaris_$lokasi SET kategori='1', jenis='1' WHERE kategori='1' AND jenis='3'");
+    mysqli_query($koneksi, "UPDATE dbm_laravel.inventaris_$lokasi SET kategori='1', jenis='3' WHERE kategori='5' AND jenis='5'");
+    mysqli_query($koneksi, "UPDATE dbm_laravel.inventaris_$lokasi SET kategori='2', jenis='3' WHERE kategori='6' AND jenis='5'");
+    mysqli_query($koneksi, "UPDATE dbm_laravel.inventaris_$lokasi SET kategori='3', jenis='3' WHERE kategori='7' AND jenis='5'");
 
     $_SESSION['success'] = "Copy Tabel Lokasi <b>$lokasi</b> Berhasil.";
     echo "<script>location.href = '/migrasi.php';</script>";
