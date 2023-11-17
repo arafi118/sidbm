c478fbe2db3641dcf830aac12c0d2ad926c83589
arafi118 <arp46301@gmail.com>
2023-11-17 11:27:14 +0700

    Urutan

--- app/Http/Controllers/PelaporanController.php
+++ app/Http/Controllers/PelaporanController.php
@@ -630,7 +630,7 @@ class PelaporanController extends Controller
         $data['rek'] = Rekening::where('kode_akun', $data['kode_akun'])->first();
         $data['transaksi'] = Transaksi::where('tgl_transaksi', 'LIKE', '%' . $tgl . '%')->where(function ($query) use ($data) {
             $query->where('rekening_debit', $data['kode_akun'])->orwhere('rekening_kredit', $data['kode_akun']);
-        })->with('user')->orderBy('tgl_transaksi', 'ASC')->orderBy('idt', 'ASC')->get();
+        })->with('user')->orderBy('tgl_transaksi', 'ASC')->orderBy('urutan', 'ASC')->orderBy('idt', 'ASC')->get();
 
         $data['saldo'] = $keuangan->saldoAwal($tgl, $data['kode_akun']);
         $data['d_bulan_lalu'] = $keuangan->saldoD($awal_bulan, $data['kode_akun']);
