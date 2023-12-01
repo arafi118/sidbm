03dae81c217d2e1e51f90260252b714a1110707b
arafi118 <arp46301@gmail.com>
2023-12-01 14:24:28 +0700

    Hak Akses

--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -1072,7 +1072,7 @@ class TransaksiController extends Controller
             $awal_bulan = ($thn - 1) . '12-31';
         }
 
-        $data['is_dir'] = (auth()->guard('web')->user()->level == 1 && auth()->guard('web')->user()->jabatan == 1) ? true : false;
+        $data['is_dir'] = (auth()->guard('web')->user()->level == 1 && (auth()->guard('web')->user()->jabatan == 1 || auth()->guard('web')->user()->jabatan == 3)) ? true : false;
 
         $data['rek'] = Rekening::where('kode_akun', $data['kode_akun'])->first();
         $data['transaksi'] = Transaksi::where('tgl_transaksi', 'LIKE', '%' . $tgl . '%')->where(function ($query) use ($data) {
