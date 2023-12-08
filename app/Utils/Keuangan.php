22fb20a1ddd5060a76e3953f185a5b051b07d591
arafi118 <arp46301@gmail.com>
2023-12-08 09:23:21 +0700

    Pembulatan angka

--- app/Utils/Keuangan.php
+++ app/Utils/Keuangan.php
@@ -14,40 +14,19 @@ class Keuangan
 {
     public static function bulatkan($angka)
     {
+        $angka = round($angka);
+
         $kec = Kecamatan::where('id', Session::get('lokasi'))->first();
         $pembulatan    = number_format($kec->pembulatan, 0, '', '');
-        $length        = strlen($pembulatan);
-        $uang        = floor($angka);
-        $bulat         = substr($pembulatan, 1);
-        if ($length <= 3) {
-            $pecahan         = substr($uang, -2);
-            $pengali        = $bulat * 2;
+        $ratusan = substr($angka, -3);
+        $nilai_tengah = $pembulatan / 2;
+        if ($ratusan < $nilai_tengah) {
+            $akhir = $angka - $ratusan;
         } else {
-            $pecahan         = substr($uang, -3);
-            $pengali        = 1000;
+            $akhir = $angka + ($pembulatan - $ratusan);
         }
 
-        if ($pembulatan > 0) {
-            if ($pecahan < $bulat and $pecahan > 0) {
-                $pembulatan = $uang + ($bulat - $pecahan);
-            } else if ($pecahan > $bulat) {
-                $pembulatan = $uang + $pengali - $pecahan;
-            } else if ($pecahan == 0 or $pecahan == $bulat) {
-                $pembulatan = $uang;
-            }
-        } else  if ($pembulatan < 0) {
-            if ($pecahan < $bulat and $pecahan > 0) {
-                $pembulatan = $uang - $pecahan;
-            } else if ($pecahan > $bulat) {
-                $pembulatan = $uang + (($bulat * 2) - $pecahan);
-            } else if ($pecahan == 0 or $pecahan == $bulat) {
-                $pembulatan = $uang;
-            }
-        } else {
-            $pembulatan = $uang;
-        }
-
-        return $pembulatan;
+        return $akhir;
     }
 
     public static function startWith($string, $startString)
