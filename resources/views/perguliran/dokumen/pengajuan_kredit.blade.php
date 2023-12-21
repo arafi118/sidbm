ecccebe907d01adb0fb582c56e972dfe367c375f
arafi118 <arp46301@gmail.com>
2023-12-21 13:24:31 +0700

    Pengajuran Kredit

--- resources/views/perguliran/dokumen/pengajuan_kredit.blade.php
+++ resources/views/perguliran/dokumen/pengajuan_kredit.blade.php
@@ -192,7 +192,8 @@
                 Sebagai bahan pertimbangan, bersama ini kami lampirkan:
             </div>
             <ol>
-                <li>Fotokopi KTP dan KK dari 0 orang anggota kelompok kami yang mengajukan kredit;</li>
+                <li>Fotokopi KTP dan KK dari {{ $pinkel->pinjaman_anggota_count }} orang anggota kelompok kami yang
+                    mengajukan kredit;</li>
                 <li>Surat Rekomendasi dari Kepala Desa/Lurah;</li>
                 <li>Pernyataan kesediaan tanggung renteng dari seluruh anggota;</li>
                 <li>Fotokopi bukti tabungan kelompok di bank;</li>
