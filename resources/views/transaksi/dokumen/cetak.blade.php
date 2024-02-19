790ef45869a682989e258c89cc2f1df636f4cb49
arafi118 <arp46301@gmail.com>
2024-02-19 14:23:38 +0700

    OR

--- resources/views/transaksi/dokumen/cetak.blade.php
+++ resources/views/transaksi/dokumen/cetak.blade.php
@@ -195,9 +195,11 @@
 
                         @if (
                             $trx->id_pinj != 0 &&
-                                ($trx->rekening_debit != '1.1.03.01' ||
+                                !(
+                                    $trx->rekening_debit != '1.1.03.01' ||
                                     $trx->rekening_debit != '1.1.03.02' ||
-                                    $trx->rekening_debit != '1.1.03.03'))
+                                    $trx->rekening_debit != '1.1.03.03'
+                                ))
                             @php
                                 $count = 3;
                             @endphp
