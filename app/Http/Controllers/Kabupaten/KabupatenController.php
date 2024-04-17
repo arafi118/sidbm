611692e7a5bf3b4388958cdce044bb86c82a0e62
arafi118 <arp46301@gmail.com>
2024-04-17 10:53:07 +0700

    Dashboard Kabupaten

--- app/Http/Controllers/Kabupaten/KabupatenController.php
+++ app/Http/Controllers/Kabupaten/KabupatenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Kabupaten;
 use App\Http\Controllers\Controller;
 use App\Models\JenisLaporan;
 use App\Models\Kecamatan;
+use App\Models\Rekening;
 use App\Models\Wilayah;
 use App\Utils\Keuangan;
 use Session;
@@ -13,8 +14,61 @@ class KabupatenController extends Controller
 {
     public function index()
     {
+        $keuangan = new Keuangan;
+        $tahun = date('Y');
+        $bulan = date('m');
+
+        $kd_prov = Session::get('kd_prov');
+        $kd_kab = Session::get('kd_kab');
+
+        $saldo_kec = [];
+        $wilayah = Wilayah::where('kode', 'like', $kd_kab . '%')->whereRaw('LENGTH(kode)=8')->with('kec')->orderBy('nama')->get();
+        foreach ($wilayah as $wl) {
+            $saldo_kec[$wl->kode] = [
+                'nama' => $wl->nama,
+                'kode' => $wl->kode,
+                'laba_rugi' => [],
+                'surplus' => 0
+            ];
+
+            if ($wl->kec) {
+                Session::put('lokasi', $wl->kec->id);
+                $laba_rugi = Rekening::where('lev1', '>=', '4')->with([
+                    'kom_saldo' => function ($query) use ($tahun, $bulan) {
+                        $query->where('tahun', $tahun)->where(function ($query) use ($bulan) {
+                            $query->where('bulan', '0')->orwhere('bulan', $bulan);
+                        });
+                    },
+                    'saldo' => function ($query) use ($tahun, $bulan) {
+                        $query->where([
+                            ['tahun', $tahun],
+                            ['bulan', ($bulan - 1)]
+                        ]);
+                    }
+                ])->orderBy('kode_akun', 'ASC')->get();
+
+                $pendapatan = 0;
+                $biaya = 0;
+                foreach ($laba_rugi as $lb) {
+                    $saldo = $keuangan->komSaldo($lb);
+                    if ($lb->lev1 == 5) {
+                        $biaya += $saldo;
+                    } else {
+                        $pendapatan += $saldo;
+                    }
+                }
+
+                $saldo_kec[$wl->kode]['laba_rugi'] = [
+                    'pendapatan' => $pendapatan,
+                    'biaya' => $biaya,
+                ];
+
+                $saldo_kec[$wl->kode]['surplus'] = $pendapatan - $biaya;
+            }
+        }
+
         $title = Session::get('nama_kab') . ' Page';
-        return view('kabupaten.index')->with(compact('title'));
+        return view('kabupaten.index')->with(compact('title', 'saldo_kec', 'keuangan'));
     }
 
     public function kecamatan($kd_kec)
