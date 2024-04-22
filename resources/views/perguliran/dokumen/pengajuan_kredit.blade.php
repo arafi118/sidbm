31c4feaa5e455d2de16eebddbbb2393c58b897ff
arafi118 <arp46301@gmail.com>
2024-04-22 11:41:59 +0700

    Permohonan Pinjaman

--- resources/views/perguliran/dokumen/pengajuan_kredit.blade.php
+++ resources/views/perguliran/dokumen/pengajuan_kredit.blade.php
@@ -86,7 +86,7 @@
         <td width="5" align="right">:</td>
         <td width="500">
             <b>
-                ______/{{ $pinkel->id }}/{{ $pinkel->jpp->nama_jpp }}/{{ Tanggal::tglRomawi($pinkel->tgl_proposal) }}
+                ______/{{ $pinkel->jpp->nama_jpp }}/{{ Tanggal::tglRomawi($pinkel->tgl_proposal) }}
             </b>
         </td>
     </tr>
