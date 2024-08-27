a0b6f8a118c283e10a5d70be1728478dc7274c28
arafi118 <arp46301@gmail.com>
2024-08-27 09:20:13 +0700

    Logo kab

--- resources/views/perguliran/dokumen/rekomendasi_kredit.blade.php
+++ resources/views/perguliran/dokumen/rekomendasi_kredit.blade.php
@@ -1,5 +1,6 @@
 @php
     use App\Utils\Tanggal;
+    $logo = $kab->id . '.png';
 @endphp
 
 <head>
@@ -78,8 +79,8 @@
             style="font-size: 11px; position: relative; top: -20px;">
             <tr class="b">
                 <td align="center">
-                    <img src="./storage/logo_kab/{{ $kab->id }}.png" width="70" alt="{{ $kab->id }}.png"
-                        style="margin-bottom: 8px;">
+                    <img src="../storage/app/public/logo_kab/{{ $logo }}" width="70"
+                        alt="{{ $logo }}.png" style="margin-bottom: 8px;">
                 </td>
                 <td align="center">
                     <div style="font-size: 18px;">
