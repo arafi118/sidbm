afecbff82872ced2d86b4d6210a6f8cf0e1ac231
arafi118 <arp46301@gmail.com>
2024-08-27 10:40:34 +0700

    Logo

--- app/Http/Controllers/SopController.php
+++ app/Http/Controllers/SopController.php
@@ -325,7 +325,7 @@ class SopController extends Controller
             'logo_kec' => 'required|image|mimes:jpg,png,jpeg|max:4096'
         ]);
 
-        if ($request->file('logo_kec')->isValid()) {
+        if ($request->hasFile('logo_kec')) {
             $extension = $request->file('logo_kec')->getClientOriginalExtension();
 
             $filename = time() . '_' . $kec->id . '_' . date('Ymd') . '.' . $extension;
