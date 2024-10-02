fcf2e6a7f05fa8a297d598d72b2413fea9febc62
arafi118 <arp46301@gmail.com>
2024-10-02 10:16:53 +0700

    Hak Akses dan Surat Pemberitahuan ke Desa

--- resources/views/admin/user/detail.blade.php
+++ resources/views/admin/user/detail.blade.php
@@ -1,32 +1,74 @@
+@php
+    use App\Utils\Tanggal;
+@endphp
+
 @extends('admin.layout.base')
 
 @section('content')
-    <div class="nav-wrapper position-relative end-0">
-        <ul class="nav nav-pills nav-fill p-1 d-none" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link mb-0 px-0 py-1 active" id="TabAksesMenu" data-bs-toggle="tab" href="#HakAksesMenu"
-                    role="tab" aria-controls="HakAksesMenu" aria-selected="true">
-                    Hak Akses Menu
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link mb-0 px-0 py-1" id="TabAksesTombol" data-bs-toggle="tab" href="#HakAksesTombol"
-                    role="tab" aria-controls="HakAksesTombol" aria-selected="false">
-                    Hak Akses Tombol
-                </a>
-            </li>
-        </ul>
-
-        <div class="tab-content mt-2">
-            <div class="tab-pane fade show active" id="HakAksesMenu" role="tabpanel" aria-labelledby="HakAksesMenu">
-                @include('admin.user.partials.akses_menu')
-            </div>
-            <div class="tab-pane fade" id="HakAksesTombol" role="tabpanel" aria-labelledby="HakAksesTombol">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="mb-0">Pengaturan Hak Akses Tombol</h5>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card position-sticky top-10" style="border-radius: 15px;">
+                <div class="card-body text-center">
+                    <div class="mt-3 mb-4">
+                        <img src="/storage/profil/{{ $user->foto }}" class="rounded-circle img-fluid"
+                            style="width: 100px;" />
                     </div>
-                    <div class="card-body pt-0" id="HakAksesTombolUser"></div>
+                    <h4 class="mb-2">
+                        {{ $user->namadepan }} {{ $user->namabelakang }}
+                    </h4>
+                    <p class="text-muted mb-4">
+                        {{ $user->l->nama_level }}
+                    </p>
+
+                    <ul class="list-group list-group-flush mb-3">
+                        <li class="list-group-item bg-transparent">
+                            {{ $user->tempat_lahir }}, {{ Tanggal::tglLatin($user->tgl_lahir) }}
+                        </li>
+                        <li class="list-group-item bg-transparent">
+                            {{ $user->hp }}
+                        </li>
+                        <li class="list-group-item bg-transparent">
+                            {{ $user->j->nama_jabatan }}
+                        </li>
+                    </ul>
+
+                    <div class="d-grid">
+                        <button type="button" class="btn btn-primary btn-lg">
+                            Edit User
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="nav-wrapper position-relative end-0">
+                <ul class="nav nav-pills nav-fill p-1 d-none" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link mb-0 px-0 py-1 active" id="TabAksesMenu" data-bs-toggle="tab"
+                            href="#HakAksesMenu" role="tab" aria-controls="HakAksesMenu" aria-selected="true">
+                            Hak Akses Menu
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link mb-0 px-0 py-1" id="TabAksesTombol" data-bs-toggle="tab" href="#HakAksesTombol"
+                            role="tab" aria-controls="HakAksesTombol" aria-selected="false">
+                            Hak Akses Tombol
+                        </a>
+                    </li>
+                </ul>
+
+                <div class="tab-content mt-2">
+                    <div class="tab-pane fade show active" id="HakAksesMenu" role="tabpanel" aria-labelledby="HakAksesMenu">
+                        @include('admin.user.partials.akses_menu')
+                    </div>
+                    <div class="tab-pane fade" id="HakAksesTombol" role="tabpanel" aria-labelledby="HakAksesTombol">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="mb-0">Pengaturan Hak Akses Tombol</h5>
+                            </div>
+                            <div class="card-body pt-0" id="HakAksesTombolUser"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
