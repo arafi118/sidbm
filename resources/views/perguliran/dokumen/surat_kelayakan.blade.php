6b8c65eae38196c7dfff4c37cc17d3fad2caabdf
nurfandi31 <fandinur241@gmail.com>
2025-06-17 09:17:32 +0700

    surat kelayakan

--- resources/views/perguliran/dokumen/surat_kelayakan.blade.php
+++ resources/views/perguliran/dokumen/surat_kelayakan.blade.php
@@ -97,9 +97,9 @@
                     </tr>
                     <tr>
                         <td>4.</td>
-                        <td>Jumlah Permohonan</td>
+                        <td>Nilai Kelayakan</td>
                         <td>:</td>
-                        <td>Rp {{ number_format($pinkel->proposal) }}</td>
+                        <td>Rp {{ number_format($pinkel->alokasi) }}</td>
                     </tr>
                     <tr>
                         <td>5.</td>
