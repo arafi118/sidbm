6a7553167cab532b1849e3b27552f3b3cd7b228f
arafi118 <arp46301@gmail.com>
2025-07-07 11:50:26 +0700

    DD

--- app/Http/Controllers/GenerateController.php
+++ app/Http/Controllers/GenerateController.php
@@ -247,6 +247,10 @@ class GenerateController extends Controller
                                 $data_penghapusan[$key]['alokasi_jasa_pinjaman'] = $alokasi_jasa_pinjaman;
                             }
 
+                            if ($_tempo_pokok == 0) {
+                                dd($pinkel);
+                            }
+
                             $pokok = Keuangan::bulatkan($alokasi_pokok_pinjaman / $_tempo_pokok);
                             $jasa = Keuangan::bulatkan($alokasi_jasa_pinjaman / $_tempo_jasa);
                             $angsuran_ke = $i + $key;
