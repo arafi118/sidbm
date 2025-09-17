a0d939f5d2ba6f2a6281b474008fd129513f7edb
arafi118 <arp46301@gmail.com>
2025-09-17 16:53:21 +0700

    Judul

--- resources/views/perguliran/dokumen/pengajuan_kredit.blade.php
+++ resources/views/perguliran/dokumen/pengajuan_kredit.blade.php
@@ -130,13 +130,6 @@
 </table>
 <table border="0" width="100%" cellspacing="0" cellpadding="0" style="font-size: 14px;">
     <tr>
-        <td colspan="3" align="center">
-            <div style="font-size: 20px;">
-                <b>SURAT PERMOHONAN KREDIT</b>
-            </div>
-        </td>
-    </tr>
-    <tr>
         <td colspan="3" height="5"></td>
     </tr>
 </table>
