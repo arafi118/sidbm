38c87c716c443f0d18981a0e246615138c994159
arafi118 <arp46301@gmail.com>
2025-12-04 10:30:41 +0700

    dump

--- app/Http/Controllers/Api/PengaturanController.php
+++ app/Http/Controllers/Api/PengaturanController.php
@@ -136,6 +136,8 @@ class PengaturanController extends Controller
             'files',
         ]);
 
+        dd($request->file('files'));
+
         $validate = Validator::make($data, [
             'files' => 'required|image|mimes:jpg,png,jpeg|max:8192',
         ]);
